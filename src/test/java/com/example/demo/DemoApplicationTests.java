d224f32531ee4892ee2a39ab7cc3ec0039af36cd
Daekwon Kang <daekwon.kang@gmail.com>
2021-02-05 10:55:25 +0900

    remove profile on testcase

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("chaos-monkey")
 class DemoApplicationTests {
 
 	@Test
